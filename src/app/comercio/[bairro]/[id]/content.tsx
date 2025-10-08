d7dd8370d9f9617fb1ec93e289c8ed2794c5406e
barcellos96 <felipebarcellos11@gmail.com>
2025-10-08 11:45:58 -0300

    fix: update ComercioDetails layout and correct navigation link text

--- src/app/comercio/[bairro]/[id]/content.tsx
+++ src/app/comercio/[bairro]/[id]/content.tsx
@@ -13,7 +13,6 @@ import { usePublicCompany } from "@/provider/company";
 import { CompanyAnalyticsContext } from "@/provider/analytics/company";
 import DefaultImage from "../../../../assets/no-img.png";
 
-
 // Função para extrair coordenadas do link do Google Maps
 function extractCoordinatesFromMapsLink(
   mapsLink: string
@@ -78,7 +77,6 @@ export default function ComercioDetails() {
 
   const [company, setCompany] = useState<any>(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [activeImage, setActiveImage] = useState(0);
   const [hasTrackedInitialView, setHasTrackedInitialView] = useState(false);
 
   useEffect(() => {
@@ -306,18 +304,16 @@ export default function ComercioDetails() {
         <div className="rounded-lg mb-8">
           <div className="flex flex-col lg:flex-row gap-6 items-start">
             {/* Coluna da esquerda com imagem e indicadores */}
-            <div className="relative w-full lg:w-5/12">
-              {/* Imagem principal */}
-              <div className="relative w-[408] h-[355px] rounded-lg overflow-hidden">
-                <Image
-                  src={company.image}
-                  alt={company.name}
-                  fill
-                  className="object-cover"
-                  priority
-                  unoptimized
-                />
-              </div>
+            {/* Imagem principal */}
+            <div className="relative w-[335px] lg:w-[480px] lg:min-w-[480px] h-[355px] rounded-lg overflow-hidden">
+              <Image
+                src={company.image}
+                alt={company.name}
+                fill
+                className="object-cover"
+                priority
+                unoptimized
+              />
             </div>
 
             {/* Detalhes do comércio */}
